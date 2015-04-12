b83c748eb2c783ad1583f22bb5839f116f38da5a
adrendon <alexis9229@hotmail.com>
2015-04-11 19:15:57 -0500

    maps

--- js/init.js
+++ js/init.js
@@ -110,11 +110,16 @@ function servicios($this) {
     jQuery('.serviciosMain').addClass('serviciosMainActive');
     jQuery('.imgBtnLeft').addClass('imgBtnLeftActive');
     jQuery('.imgBtnRight').addClass('imgBtnRightActive');
+
+    if (jQuery('.imgPage').hasClass('zIndex')) {
+        jQuery('.imgPage').removeClass('zIndex');
+    }
 }
 
 function serviciosItem($this) {
     var idElemContent = jQuery($this).parent().attr('href');
     var elemContent = jQuery(idElemContent);
+    jQuery('.imgPage').addClass('zIndex');
     if (elemContent.hasClass('contentMostrar')) {
         elemContent.removeClass('contentMostrar');
     } else {
@@ -229,6 +234,143 @@ function mapa ($this) {
     }
 }
 
+var MY_MAPTYPE_ID = 'Stylo001';
+
+function initialize() {
+  var featureOpts = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#81BDD6"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+];
+  var myGent = new google.maps.LatLng(6.257522,-75.564547);
+  var Kine = new google.maps.LatLng(6.257522,-75.564547);
+  var mapOptions = {
+    zoom: 18,
+     mapTypeControl: false,
+    zoomControl: true,
+    zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.LARGE,
+        position: google.maps.ControlPosition.LEFT_TOP,
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+    },
+    mapTypeId: MY_MAPTYPE_ID,
+    scaleControl: false,
+    streetViewControl: false,
+    center: myGent
+  }
+  var mapa001 = new google.maps.Map(document.getElementById('map-canvas001'), mapOptions);
+  var mapa002 = new google.maps.Map(document.getElementById('map-canvas002'), mapOptions);
+  var mapa003 = new google.maps.Map(document.getElementById('map-canvas003'), mapOptions);
+
+  var styledMapOptions = {
+    name: 'Stylo001'
+  };
+
+var image = 'img/14_15/marcador.png';
+  var marker001 = new google.maps.Marker({
+      position: Kine,
+      map: mapa001,
+animation: google.maps.Animation.DROP,
+      title: 'Map 01',
+icon: image
+  });
+var marker002 = new google.maps.Marker({
+      position: Kine,
+      map: mapa002,
+animation: google.maps.Animation.DROP,
+      title: 'Map 02',
+icon: image
+  });
+var marker003 = new google.maps.Marker({
+      position: Kine,
+      map: mapa003,
+animation: google.maps.Animation.DROP,
+      title: 'Map 03',
+icon: image
+  });
+
+  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+  mapa001.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+  mapa002.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+  mapa003.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+
+}
 
 window.onload = function () {
 
@@ -246,4 +388,7 @@ window.onload = function () {
         hideContebtVentajas();
         gallery();
     });
+
 };
+
+google.maps.event.addDomListener(window, 'load', initialize);
